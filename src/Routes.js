15fd7052de326bdbaaac99b55bb2c017eb5ee4c3
Vitor M. Alves <vtr.vma@hotmail.com>
2022-11-09 16:50:22 -0300

    Requisito 02 | Criando botão de iniciar o jogo

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
       </Switch>
     );
   }
