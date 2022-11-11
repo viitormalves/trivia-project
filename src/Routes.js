c50e9d977cf5e13a094da852e0d668aab66b4c99
Igor Renan <igorenansc@gmail.com>
2022-11-11 15:14:25 -0300

    requisito 16 | botão para página de ranking

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 class Routes extends Component {
   render() {
@@ -13,6 +14,7 @@ class Routes extends Component {
         <Route path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     );
   }
