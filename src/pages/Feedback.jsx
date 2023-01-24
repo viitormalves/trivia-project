cd3fd1bcf8ebe276627e763fa1f3a9bf4d62d27e
Vitor M. Alves <vtr.vma@hotmail.com>
2023-01-24 11:20:29 -0300

    Feat: Feedback.css

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,39 +2,49 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import '../css/Feedback.css';
 
 class Feedback extends Component {
   render() {
     const { history } = this.props;
     const { assertions, score } = this.props;
     return (
-      <div>
+      <>
         <Header />
-        <h3 data-testid="feedback-text">
-          {(assertions > 2) ? 'Well Done!' : 'Could be better...' }
-        </h3>
-        <p>
-          {'You got '}
-          <span data-testid="feedback-total-question">{assertions}</span>
-          {' questions right totaling '}
-          <span data-testid="feedback-total-score">{score}</span>
-          {' points'}
-        </p>
-        <button
-          type="button"
-          data-testid="btn-play-again"
-          onClick={ () => (history.push('/')) }
-        >
-          PLAY AGAIN
-        </button>
-        <button
-          type="button"
-          data-testid="btn-ranking"
-          onClick={ () => (history.push('/ranking')) }
-        >
-          SHOW RANKING
-        </button>
-      </div>
+        <div className="feedback-data">
+          <h3
+            data-testid="feedback-text"
+            className={ (assertions > 2) ? 'h3-green' : 'h3-red' }
+          >
+            {(assertions > 2) ? 'Well Done!' : 'Could be better...' }
+          </h3>
+          <p>
+            {'You got '}
+            <span data-testid="feedback-total-question">{assertions}</span>
+            {' questions right totaling '}
+            <span data-testid="feedback-total-score">{score}</span>
+            {' points'}
+          </p>
+        </div>
+        <div className="div-button">
+          <button
+            type="button"
+            data-testid="btn-play-again"
+            onClick={ () => (history.push('/')) }
+            className="play-again-button"
+          >
+            PLAY AGAIN
+          </button>
+          <button
+            type="button"
+            data-testid="btn-ranking"
+            onClick={ () => (history.push('/ranking')) }
+            className="show-ranking-button"
+          >
+            SHOW RANKING
+          </button>
+        </div>
+      </>
     );
   }
 }
